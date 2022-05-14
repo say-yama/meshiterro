da00ef4f1430f25941e83f6f51ee48d6c0b886b7
EC2 Default User <ec2-user@ip-172-31-47-176.ec2.internal>
2022-05-14 13:20:09 +0000

    [Add]user,show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
+  
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
+  resources :post_images, only:[:new, :index, :show, :create, :destroy]
+  resources :users, only:[:show, :edit, :update]
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
 end
